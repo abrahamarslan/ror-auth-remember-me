84b6a53afd25cfd1f614168aa665f031bdac039d
Abraham Arslan <arslan@abrahamarslan.com>
2021-07-26 19:56:24 +0530

    #4 - Signup and Login methods

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,17 @@
+class AuthenticationController < ApplicationController
+  def login
+
+  end
+
+  def postLogin
+    puts params.inspect
+  end
+
+  def signup
+
+  end
+
+  def postSignup
+    puts params.inspect
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   root 'home#home'
-  get 'home/home'
-  get 'home/about'
-  get 'home/contact'
+  #Home
+  get "home"          => "home#home"
+  get "about"         => "home#about"
+  get "contact"       => "home#contact"
+  #Authentication
+  get "login"         => "authentication#login"
+  post "login"        => "authentication#postLogin"
+  get "signup"        => "authentication#signup"
+  post "signup"       => "authentication#postSignup"
+  get "logout"        => "authentication#logout"
+  #User
+  resources :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
