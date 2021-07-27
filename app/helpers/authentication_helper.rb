d474e4d06611bdfd881b0969542d6efebafa2107
Abraham Arslan <arslan@abrahamarslan.com>
2021-07-28 04:23:59 +0530

    #8 - Updates

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -7,8 +7,9 @@ module AuthenticationHelper
 
   #Returns the current logged-in user (if any)
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    user_id = session[:user_id]
+    if user_id
+      @current_user ||= User.find_by(id: user_id)
     elsif cookies.signed[:user_id]
       user = User.find_by(id: cookies.signed[:user_id])
       if user && user.authenticated?(cookies[:remember_token])
@@ -24,6 +25,7 @@ module AuthenticationHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -34,6 +36,13 @@ module AuthenticationHelper
     # cookies[:user_id] = {value: user.id, expires: 20.years.from_now.utc }
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  #Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,9 +27,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  #Forget a user
+  def forget(user)
+    update_attribute(:remember_digest, nil)
+  end
+
   #Returns true if the given token matches the digest
   def authenticated?(remember_token)
-    BCrypt::Password.new(remember_token) == remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
 end
